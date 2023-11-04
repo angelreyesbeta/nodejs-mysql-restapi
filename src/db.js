10847db59202fea319bec23dc3ed6abd181592b3
Angel Reyes <angelreyesbeta@hotmail.com>
2023-11-04 01:41:23 -0500

    Fix: Se agrega puerto a la conexion db

--- src/db.js
+++ src/db.js
@@ -1,10 +1,11 @@
 import {createPool} from 'mysql2/promise'
-import { DB_DATABASE, DB_HOST, DB_PASSWORF, DB_USER } from './config.js'
+import { DB_DATABASE, DB_HOST, DB_PASSWORF, DB_PORT, DB_USER } from './config.js'
 
 export const pool = createPool({
     host: DB_HOST,
     user: DB_USER,
     password: DB_PASSWORF,
+    port:DB_PORT,
     database: DB_DATABASE
 
 })

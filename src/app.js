59bdc3a61ed8a7502bd2b3d3932c116a7f932fb8
Angel Reyes <angelreyesbeta@hotmail.com>
2023-11-04 19:36:42 -0500

    fix: Se instala cors para q la api se pueda consumir desde el front

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 import express from "express";
+import cors from 'cors';
 import employeesRoutes from './routes/employees.routes.js'
 import indexRoutes from './routes/index.routes.js'
 import './config.js'
 const app = express()
 
 app.use(express.json())
+
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
 
 app.use(indexRoutes)
 app.use('/api',employeesRoutes)
